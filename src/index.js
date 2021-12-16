d0c145da29a6e52f299e522abc31cfcdc578905e
ikiouh <hamza.ikiou@etu.umontpellier.fr>
2021-12-16 12:27:35 +0100

    affichage de toutes les recettes

--- src/index.js
+++ src/index.js
@@ -12,10 +12,24 @@ app.get('/', function (req, res) {
     res.send('B̵̛͎̹̮̲̑́̓͑̄͂̂̇̕̕͘Ó̷̡͙͚̻̾̋̈́͒̈́͠Ņ̶̬͎̭̺̞̮̖͈̖̞͚̯͉͇͙͍͒̽͒͛̓̈́̄̉̈́̚N̸̡͈̖̟̠͖̗̟̭̺̠̜̦̽͊̐̏̀̾͐̔̅̈́̂̇͝͝ͅÈ̵̟Ş̸̧̗͔̼͖̪͙̟̱͉̯̭̯͗̈́͝ ̴̢̗̻̦̪̎͋̿̈́̋̔̔̏̎͋͐̉̅́͠ͅR̴̲͊͊͋͊̊̈̂͝͝E̶̝̅͘͠C̸̝̠̞̈̓̎̐͊̈Ẹ̵̡͇̝̦̝͔̦̗̘̝͙̒̏̓͌͂̊͛̀́T̴̛̮̬̥̤͇̠̑̇͑̍͜T̸͇̩̾̀̊̾̈́̂͛̔̓͊͗̑͊͘É̴̝̜͇̫̣̙͉̬͚̹̅̔̒̓̈̽̚ͅS̷̱̤̑̓ ̷̨̛̟̝̖̱̋̂̐̿͑̈̆͗̈́́̚͝͝D̵̜̅̀̃Ę̶͕̠̘̩̍̄̑̂̓̓̍͌̾ ̴̧̗̣͍͉̟̲̉̒̍͐͐̊̌͆͒̚͜Ç̵̼̤͓̦̣̠͕̘͙̻̟͎͓̪͋͆͌̓̋̈́U̴̢̮̘͗̑̈́͗͐͐͝I̷̛͚̹͋̇͒̽̆͗́S̵̢̼͎̯̤͕̗̦̲̮̗̩̪̹̀̽̎̽͑͜͝͝I̶̧̱̣̦͉̞̙̝̞̾̆̎̑̇̅Ṋ̶̨̢̛̯͙͕͓͓̰̪̅̐̊̐̅͑̋͒͊̏̕͘E̷̢̩͙͓͖̮͕͓̥̟̫̲̓̔̈́͊̄́͌̀͆͝S̴͉̀̈͑͆͊̅̾̿̓̑̅͂͑̂̚͘')
 })*/
 
+app.use(express.json())
+
+const config = {
+    headers : {
+        'x-apikey' : '7d35e6b431fed775185712e24ba0faa1597ec'
+    }
+}
+
 app.get('/recipes', async function(req, res) {
-    res.send(axios.get('https://deepfriedrecipes-be35.restdb.io/rest/recipes'))
+    axios.get('https://deepfriedrecipes-be35.restdb.io/rest/recipes', config)
+    .then(results => {
+        res.send(results.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 })
 
-app.listen(PORT, function () {
-    console.log('Example app listening on port ' + PORT)
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000')
 })
