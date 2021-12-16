2a210a83cb0f59503cb868fb57b8393900d7909d
ikiouh <hamza.ikiou@etu.umontpellier.fr>
2021-12-16 11:54:50 +0100

    test affichage en utilisant axios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
+
+const axios = require("axios");
 const PORT = process.env.PORT || 5000 // this is very important
 
 app.get('/', function (req, res) {
     res.send('Hello the World!')
 })
 
-app.get('/recipes', function (req, res) {
+/*app.get('/recipes', function (req, res) {
     res.send('B̵̛͎̹̮̲̑́̓͑̄͂̂̇̕̕͘Ó̷̡͙͚̻̾̋̈́͒̈́͠Ņ̶̬͎̭̺̞̮̖͈̖̞͚̯͉͇͙͍͒̽͒͛̓̈́̄̉̈́̚N̸̡͈̖̟̠͖̗̟̭̺̠̜̦̽͊̐̏̀̾͐̔̅̈́̂̇͝͝ͅÈ̵̟Ş̸̧̗͔̼͖̪͙̟̱͉̯̭̯͗̈́͝ ̴̢̗̻̦̪̎͋̿̈́̋̔̔̏̎͋͐̉̅́͠ͅR̴̲͊͊͋͊̊̈̂͝͝E̶̝̅͘͠C̸̝̠̞̈̓̎̐͊̈Ẹ̵̡͇̝̦̝͔̦̗̘̝͙̒̏̓͌͂̊͛̀́T̴̛̮̬̥̤͇̠̑̇͑̍͜T̸͇̩̾̀̊̾̈́̂͛̔̓͊͗̑͊͘É̴̝̜͇̫̣̙͉̬͚̹̅̔̒̓̈̽̚ͅS̷̱̤̑̓ ̷̨̛̟̝̖̱̋̂̐̿͑̈̆͗̈́́̚͝͝D̵̜̅̀̃Ę̶͕̠̘̩̍̄̑̂̓̓̍͌̾ ̴̧̗̣͍͉̟̲̉̒̍͐͐̊̌͆͒̚͜Ç̵̼̤͓̦̣̠͕̘͙̻̟͎͓̪͋͆͌̓̋̈́U̴̢̮̘͗̑̈́͗͐͐͝I̷̛͚̹͋̇͒̽̆͗́S̵̢̼͎̯̤͕̗̦̲̮̗̩̪̹̀̽̎̽͑͜͝͝I̶̧̱̣̦͉̞̙̝̞̾̆̎̑̇̅Ṋ̶̨̢̛̯͙͕͓͓̰̪̅̐̊̐̅͑̋͒͊̏̕͘E̷̢̩͙͓͖̮͕͓̥̟̫̲̓̔̈́͊̄́͌̀͆͝S̴͉̀̈͑͆͊̅̾̿̓̑̅͂͑̂̚͘')
+})*/
+
+app.get('/recipes', async function(req, res) {
+    res.send(axios.get('https://deepfriedrecipes-be35.restdb.io/rest/recipes'))
 })
 
 app.listen(PORT, function () {
