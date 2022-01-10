2ad427754086d58d30c5fa6badbffb0cf40e4072
ikiouh <hamza.ikiou@etu.umontpellier.fr>
2022-01-10 12:33:28 +0100

    authentification

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const axios = require("axios");
+const axios = require("axios")
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const secret = 'JeNeSaisPasQuoiFairePourLeSecret'
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
 const PORT = process.env.PORT || 5000
 const config = {
     headers : {
         'x-apikey' : '7d35e6b431fed775185712e24ba0faa1597ec'
     }
 }
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secret
+}
 
 app.use(express.json())
+
+async function getUsers(){
+    const response = await axios.get('https://deepfriedrecipes-be35.restdb.io/rest/my-users', config)
+    return response.data
+}
+
+passport.use(
+    new JwtStrategy(jwtOptions, async function(payload, next) {
+        const users = await getUsers()
+        const user = users.find(user => user.email === payload.email)
+
+        if (user) {
+            next(null, user)
+        } else {
+            next(null, false)
+        }
+    })
+)
+
+app.use(passport.initialize())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
@@ -88,6 +118,29 @@ app.get('/recipes/delete/:id', async function(req, res) {
 
 app.get("*",(req,res) => {
     res.sendFile(__dirname + "/public/html/404.html")
+})
+
+// LOGIN
+app.post('/login', async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    if (!email || !password) {
+        res.status(401).json({ error: 'Email or password was not provided.' })
+        return
+    }
+
+    const users = await getUsers()
+    const user = users.find(user => user.email === email)
+
+    if (!user || user.password !== password) {
+        res.status(401).json({ error: 'Email / password do not match.' })
+        return
+    }
+
+    const userJwt = jwt.sign({ email: user.email }, secret)
+
+    res.json({ jwt: userJwt })
 })
 
 app.listen(PORT, function () {
